16824968421b6f1e3fcce7375402af852b9b3ddd
Werter12 <samofan77@gmail.com>
2019-07-26 17:43:42 +0300

    Add prettier and pre commit hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,12 @@
-
-import * as React from "react";
+import * as React from 'react';
 
 class App extends React.Component<{}, {}> {
   public render() {
     return (
-    <div>
-      <h1>Welcome to React with Typescript</h1>
-      <p>The color of this page is: Red</p>
-    </div>
+      <div>
+        <h1>Welcome to React with Typescript</h1>
+        <p>The color of this page is: Red</p>
+      </div>
     );
   }
 }
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,5 +1,6 @@
-import { storiesOf } from "@storybook/react";
-import * as React from "react";
-import { App } from "../src/index";
-storiesOf("TypeScript and Storybook", module)
-  .add("Sample Widget", () => <App/>);
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { App } from '../src/index';
+storiesOf('TypeScript and Storybook', module).add('Sample Widget', () => (
+  <App />
+));
