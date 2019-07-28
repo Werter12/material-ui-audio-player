b3ebf875119ae78d276cf62624332a5a9b4adeec
Werter12 <samofan77@gmail.com>
2019-07-28 17:43:10 +0300

    Update tslint rule

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,7 +1,45 @@
 import { withInfo } from '@storybook/addon-info';
-import { addDecorator, configure } from '@storybook/react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
+import { themes } from '@storybook/theming';
 
-addDecorator(withInfo({ inline: true, header: false }));
+addParameters({
+  options: {
+    brandTitle: 'material-ui-audio-player',
+    theme: themes.light
+  }
+});
+
+addDecorator(
+  withInfo({
+    styles: {
+      header: {
+        h1: {
+          marginRight: '20px',
+          fontSize: '25px',
+          margin: '0px -8px',
+          padding: '0px 8px',
+          display: 'inline'
+        },
+        body: {
+          paddingTop: 0,
+          paddingBottom: 0
+        },
+        h2: {
+          display: 'inline',
+          color: '#999'
+        }
+      },
+      infoBody: {
+        backgroundColor: '#eee',
+        padding: '0px 20px',
+        margin: '0px -8px',
+        lineHeight: '2'
+      }
+    },
+    inline: true,
+    source: false
+  })
+);
 
 const req = require.context('../stories', true, /.stories.tsx$/);
 function loadStories() {
