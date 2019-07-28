41d3bc8ef2fc6eac4b6b64ac402134f8846204ae
Werter12 <samofan77@gmail.com>
2019-07-28 23:09:52 +0300

    Add player base structure

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -15,28 +15,8 @@ addDecorator(withA11y);
 addDecorator(
   withInfo({
     styles: {
-      header: {
-        h1: {
-          marginRight: '20px',
-          fontSize: '25px',
-          margin: '0px -8px',
-          padding: '0px 8px',
-          display: 'inline'
-        },
-        body: {
-          paddingTop: 0,
-          paddingBottom: 0
-        },
-        h2: {
-          display: 'inline',
-          color: '#999'
-        }
-      },
-      infoBody: {
-        backgroundColor: '#eee',
-        padding: '0px 20px',
-        margin: '0px -8px',
-        lineHeight: '2'
+      infoStory: {
+        minHeight: '100px'
       }
     },
     inline: true,
--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,21 +1,87 @@
-import { Grid } from '@material-ui/core';
+import { Grid, Paper, Slider } from '@material-ui/core';
+import { PlayCircleFilledWhite, VolumeOff, VolumeUp } from '@material-ui/icons';
+import { withStyles } from '@material-ui/styles';
+import cx from 'classnames';
 import * as React from 'react';
+
+export interface IAudioPlayerClassNameProps {
+  root: string;
+  // loopIcon: string;
+  // playIcon: string;
+  // muteIcon: string;
+  // slider: string;
+  // track: string;
+  // thumb: string;
+  // text: string;
+}
 
 interface IAudioPlayerProps {
   src: string;
+  classes: any;
+  rounded?: boolean;
+  elevation?: number;
+  classNames?: Partial<IAudioPlayerClassNameProps>;
 }
 
-function AudioPlayer({ src }: IAudioPlayerProps) {
+export const styles = (theme: any) => {
+  const elevations = {};
+  theme.shadows.forEach((shadow, index) => {
+    elevations[`elevation${index}`] = {
+      boxShadow: shadow
+    };
+  });
+
+  return {
+    root: {
+      backgroundColor: theme.palette.background.paper,
+      color: theme.palette.text.primary,
+      transition: theme.transitions.create('box-shadow')
+    },
+    rounded: {
+      borderRadius: theme.shape.borderRadius
+    },
+    ...elevations
+  };
+};
+
+const AudioPlayer: React.FunctionComponent<IAudioPlayerProps> = ({
+  src,
+  classes,
+  rounded = true,
+  elevation = 1,
+  classNames = {}
+}) => {
+  const player = React.useRef<HTMLAudioElement | null>(null);
   return (
     <>
-      <audio
-      // ref={node => (this.player = node)}
-      >
+      <audio ref={player}>
         <source src={src} />
       </audio>
-      <Grid>Audio player</Grid>
+      <Grid
+        container={true}
+        spacing={4}
+        component={Paper}
+        className={cx(
+          classes.root,
+          classes[`elevation${elevation}`],
+          {
+            [classes.rounded]: !rounded
+          },
+          classNames.root
+        )}
+      >
+        <Grid item={true}>
+          <PlayCircleFilledWhite />
+        </Grid>
+        <Grid item={true}>
+          <VolumeUp />
+        </Grid>
+        <Grid item={true}>
+          <Slider />
+        </Grid>
+      </Grid>
     </>
   );
-}
+};
 
-export default AudioPlayer;
+export default withStyles(styles, { name: 'AudioPlayer' })(AudioPlayer);
