a2d32c0c69df4a0473fd1c5834e99e6a25dbee76
Werter12 <samofan77@gmail.com>
2019-07-28 17:43:17 +0300

    Add addon info support and theming for storybook

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,6 +1,22 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { App } from '../src/index';
-storiesOf('TypeScript and Storybook', module).add('Sample Widget', () => (
-  <App />
-));
+storiesOf('Material Ui', module).add('AudioPlayer', () => <App />, {
+  info: {
+    text: `
+### Notes
+
+light button seen on <https://zpl.io/aM49ZBd>
+
+### Usage
+~~~js
+<App
+  label={text('label', 'Enroll')}
+  disabled={boolean('disabled',false)}
+  onClick={() => alert('hello there')}
+/>
+~~~
+
+`
+  }
+});
