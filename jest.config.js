2882c795e6a6e978d83f3f1059444ce0ac6eb199
Werter12 <samofan77@gmail.com>
2019-08-10 19:22:00 +0300

    Move setup enzym

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
   testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFilesAfterEnv: ['./src/setupEnzyme.ts'],
+  setupFilesAfterEnv: ['./src/tests/setupEnzyme.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   globals: {
     'ts-jest': {
