29cb83885ad407c66fc7aaf1c4128e3781c9e13e
Werter12 <samofan77@gmail.com>
2019-08-10 18:14:10 +0300

    Several configuration changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,14 @@ const path = require('path');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.min.js',
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
-    rules: [
-      { test: /\.(ts|tsx)?$/,  loader: 'awesome-typescript-loader' }
-    ]
+    rules: [{ test: /\.(ts|tsx)?$/, loader: 'awesome-typescript-loader' }]
   }
 };
